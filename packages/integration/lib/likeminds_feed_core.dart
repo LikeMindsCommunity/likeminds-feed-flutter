bbf1c7d7276b91e9dbd939368bf973a75c35d6d6
Anurag Tyagi <anurag.tyagi@likeminds.community>
2024-01-17 13:54:38 +0530

    chore(UI): added export for UI in core

--- packages/integration/lib/likeminds_feed_core.dart
+++ packages/integration/lib/likeminds_feed_core.dart
@@ -13,6 +13,8 @@ import 'package:media_kit/media_kit.dart';
 export 'package:likeminds_feed_flutter_core/src/views/views.dart';
 export 'package:likeminds_feed_flutter_core/src/utils/constants/constants.dart';
 export 'package:likeminds_feed_flutter_core/src/bloc/bloc.dart';
+export 'package:likeminds_feed_flutter_core/src/convertors/model_convertor.dart';
+export 'package:likeminds_feed_flutter_ui/likeminds_feed_flutter_ui.dart';
 
 class LMFeedCore {
   late final LMFeedClient lmFeedClient;
