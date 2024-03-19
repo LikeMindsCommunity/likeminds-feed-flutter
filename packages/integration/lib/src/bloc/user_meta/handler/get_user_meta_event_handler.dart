5fbbdf2cd05a966e7234728f6ac54b893c351355
TyagiLikeMinds <anurag.tyagi@likeminds.community>
2024-03-19 18:48:04 +0530

    fix: user feed meta topics issue

--- packages/integration/lib/src/bloc/user_meta/handler/get_user_meta_event_handler.dart
+++ packages/integration/lib/src/bloc/user_meta/handler/get_user_meta_event_handler.dart
@@ -1,25 +1,29 @@
 part of '../user_meta_bloc.dart';
 
 getUserMetaEventHandler(
-      LMFeedUserMetaGetEvent event, Emitter<LMFeedUserMetaState> emit) async {
-    emit(LMFeedUserMetaLoadingState());
-    try {
-      final response = await LMFeedCore.instance.lmFeedClient.getUserFeedMeta(
-          (GetUserFeedMetaRequestBuilder()..uuid(event.uuid)).build());
+    LMFeedUserMetaGetEvent event, Emitter<LMFeedUserMetaState> emit) async {
+  emit(LMFeedUserMetaLoadingState());
+  try {
+    final response = await LMFeedCore.instance.lmFeedClient.getUserFeedMeta(
+        (GetUserFeedMetaRequestBuilder()..uuid(event.uuid)).build());
 
-      final user = response.users?.entries.first.value;
-      final userViewData = LMUserViewDataConvertor.fromUser(
-        user!,
-        widgets: response.widgets,
-      );
-      emit(LMFeedUserMetaLoadedState(
-        user: userViewData,
-        postsCount: response.postsCount,
-        commentsCount: response.commentsCount,
-      ));
-    } on Exception catch (e, stackTrace) {
-       LMFeedLogger.instance.handleException(e, stackTrace);
-      emit(LMFeedUserMetaErrorState(
-          message: e.toString(), ));
-    }
+    final user = response.users?.entries.first.value;
+    final userViewData = LMUserViewDataConvertor.fromUser(
+      user!,
+      widgets: response.widgets,
+      userTopics: response.userTopics,
+      topics: response.topics,
+    );
+
+    emit(LMFeedUserMetaLoadedState(
+      user: userViewData,
+      postsCount: response.postsCount,
+      commentsCount: response.commentsCount,
+    ));
+  } on Exception catch (e, stackTrace) {
+    LMFeedLogger.instance.handleException(e, stackTrace);
+    emit(LMFeedUserMetaErrorState(
+      message: e.toString(),
+    ));
   }
+}
