9b853998a3fd625ec97313adeb606d0556f44fdc
Anuj <anuj.kumar@likeminds.community>
2024-11-18 12:09:13 +0530

    chore : removed commented code

--- example/lib/builder/example_widget_utility.dart
+++ example/lib/builder/example_widget_utility.dart
@@ -7,27 +7,8 @@ class ExampleWidgetUtility extends LMFeedWidgetUtility {
       BuildContext context, LMFeedPostWidget post, LMPostViewData postViewData,
       {LMFeedWidgetSource source = LMFeedWidgetSource.universalFeed}) {
     return post.copyWith(
-      style: post.style?.copyWith(
-        backgroundColor: Colors.red,
-      )
-    );
+        style: post.style?.copyWith(
+      backgroundColor: Colors.red,
+    ));
   }
-  // @override
-  // Widget postReviewBannerBuilder(BuildContext context,
-  //     LMFeedPostReviewBanner postReviewBanner, LMPostViewData postViewData) {
-  //   // TODO: implement postReviewBannerBuilder
-  //   return Card(
-  //     child: Row(
-  //       children: [
-  //         if (postReviewBanner.reviewStatusIcon != null)
-  //           postReviewBanner.reviewStatusIcon!,
-  //         const SizedBox(width: 10.0),
-  //         if (postReviewBanner.reviewStatusText != null)
-  //           postReviewBanner.reviewStatusText!,
-  //         const Spacer(),
-  //         if (postReviewBanner.infoIcon != null) postReviewBanner.infoIcon!,
-  //       ],
-  //     ),
-  //   );
-  // }
 }
