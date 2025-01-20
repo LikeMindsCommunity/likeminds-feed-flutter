b8be709e99651c90d3ee1a31d707e0acd97662d1
Anuj <anuj.kumar@likeminds.community>
2025-01-20 19:03:40 +0530

    feat: added method to get height and width of video file

---
+++ packages/integration/lib/src/utils/media/video/video_info.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class LMFeedVideoInfo {
+  int? width;
+  int? height;
+  double? duration;
+  int? fileSize;
+  LMFeedVideoInfo({
+    this.width,
+    this.height,
+    this.duration,
+    this.fileSize,
+  });
+}
--- packages/integration/lib/src/utils/media/video/video_utils.dart
+++ packages/integration/lib/src/utils/media/video/video_utils.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/foundation.dart';
+import 'package:likeminds_feed_flutter_core/src/utils/media/video/video_info.dart';
 import 'package:video_compress/video_compress.dart';
 
-class LMFeedVideoThumbnail {
+class LMFeedVideoUtils {
   /// Get thumbnail of video file
   /// [path] : path of video file
   /// [position] : position of thumbnail in video
   /// [quality] : quality of thumbnail
   /// Returns path of thumbnail file
-  static Future<String?> thumbnailFile({
+  static Future<String?> getThumbnailFile({
     required String path,
     int position = 0,
     int quality = 40,
@@ -19,6 +20,25 @@ class LMFeedVideoThumbnail {
         position: position,
       );
       return thumbnail.path;
+    } on Exception catch (e) {
+      debugPrint("Error in getting thumbnail: $e");
+      return null;
+    }
+  }
+
+  static Future<LMFeedVideoInfo?> getVideoInfo({
+    required String path,
+  }) async {
+    try {
+      final info = await VideoCompress.getMediaInfo(
+        path,
+      );
+      return LMFeedVideoInfo(
+        duration: info.duration,
+        width: info.width,
+        height: info.height,
+        fileSize: info.filesize,
+      );
     } on Exception catch (e) {
       debugPrint("Error in getting thumbnail: $e");
       return null;
