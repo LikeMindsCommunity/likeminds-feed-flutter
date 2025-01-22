345cf025fa3cb0f044729bfd0180987a4fdb7530
Anuj <anuj.kumar@likeminds.community>
2025-01-22 13:14:31 +0530

    chore: added comments in video utils class

--- packages/integration/lib/src/utils/media/video/video_metadata.dart
+++ packages/integration/lib/src/utils/media/video/video_metadata.dart
@@ -1,9 +1,21 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
+/// {@template lm_feed_video_metadata}
+/// Video metadata class to store video metadata
+/// like width, height, duration and file size
+/// {@endtemplate}
 class LMFeedVideoMetaData {
+  /// Width of video
   int? width;
+
+  /// Height of video
   int? height;
+
+  /// Duration of video
   double? duration;
+
+  /// File size of video
   int? fileSize;
+
+  /// {@macro lm_feed_video_metadata}
   LMFeedVideoMetaData({
     this.width,
     this.height,
--- packages/integration/lib/src/utils/media/video/video_utils.dart
+++ packages/integration/lib/src/utils/media/video/video_utils.dart
@@ -2,6 +2,12 @@ import 'package:flutter/foundation.dart';
 import 'package:likeminds_feed_flutter_core/src/utils/media/video/video_metadata.dart';
 import 'package:video_compress/video_compress.dart';
 
+
+/// {@template lm_feed_video_utils}
+/// A utility class to handle video related operations
+/// getThumbnailFile: Get thumbnail of video file
+/// getVideoMetaData: Get metadata of video file
+/// {@endtemplate}
 class LMFeedVideoUtils {
   /// Get thumbnail of video file
   /// [path] : path of video file
@@ -26,17 +32,24 @@ class LMFeedVideoUtils {
     }
   }
 
+/// {@template lm_feed_video_utils}
+/// Get metadata of video file
+/// [path] : path of video file
+/// Returns [LMFeedVideoMetaData] object
+/// Returns null if metadata is not available
+/// {@endtemplate}
   static Future<LMFeedVideoMetaData?> getVideoMetaData({
     required String? path,
   }) async {
+    // check if path is null
     if (path == null) return null;
     try {
+      // get media info
       final info = await VideoCompress.getMediaInfo(
         path,
       );
       // convert duration to seconds
       double? duration = info.duration != null ? info.duration! / 1000 : null;
-
       return LMFeedVideoMetaData(
         duration: duration,
         width: info.width,
